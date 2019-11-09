e0d7b7561cac8b357e661b7cba460a1491a89150
Miguel Caceres <mcaceres@uni-bremen.de>
2019-11-09 19:24:06 +0100

    Add store repository

--- backend/backend-modul/src/main/java/de/profoethker/backendmodul/controller/PQController.java
+++ backend/backend-modul/src/main/java/de/profoethker/backendmodul/controller/PQController.java
@@ -8,20 +8,22 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.google.gson.Gson;
 
 import de.profoethker.backendmodul.dao.PQDao;
+import de.profoethker.backendmodul.model.CheckPA;
 import de.profoethker.backendmodul.model.PQ;
-import de.profoethker.backendmodul.rest.RestfullController;
 
 @RestController
 public class PQController {
 
 	private final PQDao pqDao;
 
-	Logger logger = LoggerFactory.getLogger(RestfullController.class);
+	Logger logger = LoggerFactory.getLogger(PQController.class);
 
 	@Autowired
 	public PQController(PQDao pq) {
@@ -49,6 +51,15 @@ public class PQController {
 		} else {
 			return null;
 		}
+	}
+
+	@PostMapping("/api/p/store")
+	@CrossOrigin
+	public void storePQ(@RequestBody CheckPA checkPA) {
+		PQ toStore = pqDao.getOne(checkPA.getQuestionID());
+		toStore.setPersona(checkPA.getPersonaSelectionID());
+		pqDao.save(toStore);
+
 	}
 
 	public PQ getRandomPQ(List<PQ> all) {
---
+++ backend/backend-modul/src/main/java/de/profoethker/backendmodul/model/CheckPA.java
@@ -0,0 +1,24 @@
+package de.profoethker.backendmodul.model;
+
+public class CheckPA {
+
+	private Integer questionID;
+	private Integer personaSelectionID;
+
+	public Integer getQuestionID() {
+		return questionID;
+	}
+
+	public void setQuestionID(Integer questionID) {
+		this.questionID = questionID;
+	}
+
+	public Integer getPersonaSelectionID() {
+		return personaSelectionID;
+	}
+
+	public void setPersonaSelectionID(Integer personaSelectionID) {
+		this.personaSelectionID = personaSelectionID;
+	}
+
+}
