996abefbf222b7cae35ad48dd178add13c17161a
Miguel Caceres <mcaceres@uni-bremen.de>
2019-11-09 23:17:07 +0100

    Add Hash512 to encrypt data

--- backend/backend-modul/src/main/java/de/profoethker/backendmodul/controller/AccountController.java
+++ backend/backend-modul/src/main/java/de/profoethker/backendmodul/controller/AccountController.java
@@ -1,8 +1,11 @@
 package de.profoethker.backendmodul.controller;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 import java.util.List;
-import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +13,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.gson.Gson;
+
+import de.profoethker.backendmodul.cybersecurity.Hash512;
 import de.profoethker.backendmodul.dao.AccountDao;
 import de.profoethker.backendmodul.model.Account;
 
@@ -17,6 +23,8 @@ import de.profoethker.backendmodul.model.Account;
 public class AccountController {
 
 	private final AccountDao accountDao;
+
+	Logger logger = LoggerFactory.getLogger(AccountController.class);
 
 	@Autowired
 	public AccountController(AccountDao accountdao) {
@@ -26,22 +34,31 @@ public class AccountController {
 	@PostMapping("/api/register")
 	@CrossOrigin
 	public boolean register(@RequestBody Account account) {
-		// TODO
-		return true;
+		try {
+			Account createAccount = new Account();
+			createAccount.setUsername(account.getUsername());
+			createAccount.setEmail(account.getEmail());
+			// Cipher to encrypt password
+			createAccount.setPassword(Hash512.encryptData(account.getPassword()));
+			accountDao.save(createAccount);
+			logger.info("Account Stored success!");
+			return true;
+		} catch (NoSuchAlgorithmException | NoSuchProviderException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 
 	@GetMapping(value = "/api/account/all", produces = "aplication/json")
 	@CrossOrigin
-	public List<Account> getAllAccounts() {
+	public String getAllAccounts() {
 		List<Account> accountsAsList = accountDao.findAll();
-		
-		List<Account> filtered = accountsAsList.stream().filter(x -> x.getUsername() != null)
-				.filter(y -> y.getEmail() != null).collect(Collectors.toList());
-		
-		// TODO DOnt give password!!!! 
-		
-		
-		return filtered;
+		for (int i = 0; i < accountsAsList.size(); i++) {
+			accountsAsList.get(i).setPassword(null);
+		}
+		Gson gson = new Gson();
+		String json = gson.toJson(accountsAsList);
+		return json;
 
 	}
 
