8ad988fc098d800f6498a90c15d5be05033aabac
Miguel Caceres <mcaceres@uni-bremen.de>
2019-11-10 12:46:01 +0100

    Add validation

--- frontend/assets/js/register.js
+++ frontend/assets/js/register.js
@@ -1,12 +1,14 @@
 $( document ).ready(function() {
     $("#load").hide();
     $("#result-icon").hide();
+    $('#error-icon').hide();
 });
 
 const ip_address = '192.168.178.43:7000'
 
 function bar(){
     $("#load").show();
+    $('#error-icon').hide();
     setTimeout(sendEvent, 1000);
 }
 
@@ -14,7 +16,8 @@ function sendEvent(){
     const username =  $( "#username").val();
     const email = $("#email").val();
     const password = $("#pass").val();
-
+    const passcon = $("#pass-confirm").val();
+    if(username && username != '' && email != '' && password != '' &&  passcon != ''){
         $.ajax({
             url: "http://"+ ip_address + "/api/register/",
             type: 'POST',
@@ -29,7 +32,10 @@ function sendEvent(){
         $("#load").hide();
         $("#result-icon").show();
         setTimeout(browserforward, 1000);
-
+    }else {
+        $("#load").hide();
+        $('#error-icon').show();
+    }
 }
 function browserforward(){
     document.location.replace('game.html');
